ed1900c9174842521cd2a9b67c696919d68e33ea
chenFlyingkite <30795482+chenFlyingkite@users.noreply.github.com>
2018-01-25 02:22:02 +0800

    Add the parsing cards

--- app/src/main/java/com/flyingkite/mytoswiki/MainActivity.java
+++ app/src/main/java/com/flyingkite/mytoswiki/MainActivity.java
@@ -1,11 +1,66 @@
 package com.flyingkite.mytoswiki;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
+import com.flyingkite.library.IOUtil;
+import com.flyingkite.library.Say;
+import com.flyingkite.mytoswiki.data.TosCard;
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class MainActivity extends BaseActivity {
+    private static final String cardJson = "card.json";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        parseCards();
+    }
+
+    private TosCard[] parseCards() {
+        Say.Log("parseCards");
+        Gson gson = new Gson();
+        Reader reader = null;
+        TosCard[] cards = null;
+        try {
+            reader = getReader(cardJson);
+            if (reader != null) {
+                cards = gson.fromJson(reader, TosCard[].class);
+            }
+
+            Say.Log("%s cards", cards == null ? 0 : cards.length);
+        } finally {
+            IOUtil.closeIt(reader);
+        }
+        return cards;
+    }
+
+
+    private InputStreamReader getReader(String assetFile) {
+        AssetManager am = getAssets();
+        try {
+            return new InputStreamReader(am.open(assetFile), "UTF-8");
+            //return new InputStreamReader(new FileInputStream(file), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private InputStreamReader getReader(File file) {
+        try {
+            return new InputStreamReader(new FileInputStream(file), "UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/flyingkite/mytoswiki/data/TosCard.java
@@ -0,0 +1,189 @@
+package com.flyingkite.mytoswiki.data;
+
+import com.google.gson.annotations.SerializedName;
+
+public class TosCard {
+
+////////////////
+// Basic Profile
+////////////////
+    @SerializedName("display_num")
+    public String display_num;
+
+    @SerializedName("icon_name")
+    public String icon_name;
+
+    @SerializedName("icon_url")
+    public String icon_url;
+
+    @SerializedName("imageId")
+    public String imageId;
+
+    @SerializedName("img_url")
+    public String img_url;
+
+////////////////
+// Basic Profile, Common selection
+////////////////
+
+    @SerializedName("number")
+    public int number;
+
+    @SerializedName("starCount")
+    public int starCount;
+
+    @SerializedName("name")
+    public String name;
+
+    @SerializedName("ethnicity")
+    public String ethnicity;
+
+    @SerializedName("series")
+    public String series;
+
+    @SerializedName("property")
+    public String property;
+
+    @SerializedName("spacing")
+    public int spacing;
+
+////////////////
+// Levels & Experience
+////////////////
+
+    @SerializedName("levelMaxExperience")
+    public int levelMaxExperience;
+
+    @SerializedName("feedExperience")
+    public int feedExperience;
+
+    @SerializedName("levelInit")
+    public int levelInit;
+
+    @SerializedName("levelInitHP")
+    public int levelInitHP;
+
+    @SerializedName("levelInitAttack")
+    public int levelInitAttack;
+
+    @SerializedName("levelInitResilience")
+    public int levelInitResilience;
+
+    @SerializedName("levelInitTotal")
+    public int levelInitTotal;
+
+    @SerializedName("levelMax")
+    public int levelMax;
+
+    @SerializedName("levelMaxHP")
+    public int levelMaxHP;
+
+    @SerializedName("levelMaxAttack")
+    public int levelMaxAttack;
+
+    @SerializedName("levelMaxResilience")
+    public int levelMaxResilience;
+
+    @SerializedName("levelMaxTotal")
+    public int levelMaxTotal;
+
+////////////////
+// Levels & Experience - All max
+////////////////
+
+    @SerializedName("allMaxHP")
+    public int allMaxHP;
+    @SerializedName("allMaxAttack")
+    public int allMaxAttack;
+    @SerializedName("allMaxResilience")
+    public int allMaxResilience;
+    @SerializedName("allMaxTotal")
+    public int allMaxTotal;
+
+////////////////
+// Skill - Normal
+////////////////
+
+    @SerializedName("skillActivityName")
+    public String skillActivityName;
+
+    @SerializedName("skillActivityDesc")
+    public String skillActivityDesc;
+
+    @SerializedName("cdMin")
+    public String cdMin;
+
+    @SerializedName("cdMax")
+    public String cdMax;
+
+    @SerializedName("skillActivityName2")
+    public String skillActivityName2;
+
+    @SerializedName("skillActivityDesc2")
+    public String skillActivityDesc2;
+
+    @SerializedName("cdMin2")
+    public String cdMin2;
+
+    @SerializedName("cdMax2")
+    public String cdMax2;
+
+////////////////
+// Skill - Leader
+////////////////
+
+    @SerializedName("skillLeaderName")
+    public String skillLeaderName;
+
+    @SerializedName("skillLeaderDesc")
+    public String skillLeaderDesc;
+
+
+////////////////
+// Evolutions card number & required card materials
+////////////////
+
+    @SerializedName("beforeNumber")
+    public String beforeNumber;
+
+    @SerializedName("afterNumber")
+    public String afterNumber;
+
+    @SerializedName("evolution1")
+    public String evolution1;
+
+    @SerializedName("evolution2")
+    public String evolution2;
+
+    @SerializedName("evolution3")
+    public String evolution3;
+
+    @SerializedName("evolution4")
+    public String evolution4;
+
+    @SerializedName("evolution5")
+    public String evolution5;
+
+
+////////////////
+// Others
+////////////////
+
+    @SerializedName("cardFall")
+    public String cardFall;
+
+    @SerializedName("sublimation_num")
+    public String sublimation_num;
+
+    @SerializedName("sublimation_name")
+    public String sublimation_name;
+
+    @SerializedName("liberation_name")
+    public String liberation_name;
+
+    @SerializedName("hell_name")
+    public String hell_name;
+
+    @SerializedName("reincarnation_name")
+    public String reincarnation_name;
+}
